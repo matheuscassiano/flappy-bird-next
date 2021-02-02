20aba421a4728342fef464a42eba69fd7266f67a
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-02-02 13:18:27 -0300

    Adding Login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,40 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import styled from 'styled-components'
 
-const Text = styled.h1`
+const Container = styled.main`
   width: 100%;
   height: 100vh;
+  padding: 2em;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-weight: 300;
 `
+
+const Input = styled.input`
+  width: 100%;
+  padding: 1em;
+  margin: .25em 0;
+  border-radius: 1em;
+`
+
+const Button = styled.button`
+  width: 100%;
+  padding: 1em;
+  margin: 1em 0;
+  border-radius: 1em;
+`
+
 const Home: React.FC<AppProps> = () => {
-  return <Text>My Typescript Page</Text>
+  return (
+    <Container>
+      <form>
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="password" name="password" placeholder="Senha" />
+        <Button>Login</Button>
+      </form>
+    </Container>
+  )
 }
 
 export default Home
