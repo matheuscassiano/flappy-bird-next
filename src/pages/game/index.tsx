11bcaebdbddeef06e5785b17e19449b2a8620014
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-02-02 11:06:42 -0300

    first commit from template NextJs

---
+++ src/pages/game/index.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { Text } from './styles'
+
+const Home: React.FC<AppProps> = () => {
+  return (
+    <canvas></canvas>
+  )
+}
+
+export default Home
---
+++ src/pages/game/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Text = styled.h1`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 300;
+`
